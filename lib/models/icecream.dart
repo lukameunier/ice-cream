1f81497632032a129790260195cfaac8d3c728de
Luka Meunier <luka.meunier@univ-rouen.fr>
2025-12-08 09:33:42 +0100

    feat(models): add IceCream class

---
+++ lib/models/icecream.dart
@@ -0,0 +1,10 @@
+class Icecream {
+  String flavour;
+  String flavourName;
+  int quantity;
+  String containing;
+  String topping;
+  double price;
+
+  Icecream(this.flavour, this.flavourName, this.quantity, this.containing, this.topping, this.price);
+}
