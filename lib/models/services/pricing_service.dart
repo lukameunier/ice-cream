835d1de182099d7c0d7bd84a2c32447d1db4fc2b
Luka Meunier <luka.meunier@univ-rouen.fr>
2025-12-13 02:45:06 +0100

    feat(architecture): add the princing table to the project

---
+++ lib/models/services/pricing_service.dart
@@ -0,0 +1,9 @@
+class PricingService {
+
+  static const Map<int, double> scoopPrices = {
+    1: 1.50, 2: 3.00, 3: 4.00, 4: 5.00, 5: 5.50,
+  };
+
+  static const double conePrice = 1.00;
+  static const double cupPrice = 0.00;
+}
