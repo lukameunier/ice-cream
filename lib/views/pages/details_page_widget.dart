a5e3b1df5dd3d27cb2089a9ad8fcebf725fbe3b3
Luka Meunier <luka.meunier@univ-rouen.fr>
2025-12-15 09:54:03 +0100

    feat(details): create the details page widget

--- lib/views/pages/details_page_widget.dart
+++ lib/views/pages/details_page_widget.dart
@@ -11,13 +11,66 @@ class DetailsPageWidget extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Padding(padding: const EdgeInsets.all(16.0),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(flavour.name, style: TextStyle(fontSize: 35)),
-              ],
-          )
+              Row(
+                children: [
+                  Image(image: AssetImage(flavour.imagePath), height: 200),
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: Text(
+                      "${flavour.name} flavour is empty",
+                      style: TextStyle(fontSize: 30),
+                      maxLines: 3,
+                      softWrap: true,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        labelText: "Signature",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                    ),
+                    onPressed: () {},
+                    child: const Text("Order"),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 16),
+
+              Row(
+                children: [
+                  Spacer(),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                    ),
+                    onPressed: () {},
+                    child: Text("Check all items"),
+                  ),
+                  Spacer(),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
